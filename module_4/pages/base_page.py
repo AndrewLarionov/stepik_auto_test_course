dfe089eef44fdc9ea1dc360563588b42898ec3fc
Andrey <tomurban1990@gmail.com>
2022-08-10 22:11:49 +0300

    add MainPage first test

--- module_4/pages/base_page.py
+++ module_4/pages/base_page.py
@@ -1,11 +1,21 @@
 from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
 
 
 class BasePage:
 
-    def __init__(self, browser: webdriver.Chrome, url: str) -> None:
-        self.browser = browser
-        self.url = url
+    def __init__(self, browser: webdriver.Chrome, url: str, timeout: int = 10) -> None:
+        self.browser: webdriver.Chrome = browser
+        self.url: str = url
+        self.browser.implicitly_wait(timeout)
 
     def open(self):
         self.browser.get(self.url)
+
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except NoSuchElementException:
+            return False
+
+        return True
